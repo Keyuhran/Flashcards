af3a466fe8b1cb4bc007b93bca38d8a759771168
Keyuhran <148024623+Keyuhran@users.noreply.github.com>
2025-07-17 13:39:40 +0800

    update import supabase

--- models/flashcard.js
+++ models/flashcard.js
@@ -1,7 +1,7 @@
 const path = require('path');
 require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 
-const { createClient } = require('@supabase/supabase-js');
+const { createClient } = require (supabase)
 
 const supabaseUrl = 'https://gnhhkqqahqwdfszpiyfb.supabase.co';
 const supabaseKey = process.env.SUPABASE_KEY;
